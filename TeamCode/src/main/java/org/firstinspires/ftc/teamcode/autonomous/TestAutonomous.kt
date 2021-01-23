e40b464f05c3e172198c0b15f39f877b03b1116c
greenpizza1203 <konakarthik12@gmail.com>
2021-01-23 12:04:57 -0500

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAutonomous.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAutonomous.kt
@@ -11,6 +11,8 @@ import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.Servo
 
 import com.qualcomm.robotcore.util.ElapsedTime
+import org.firstinspires.ftc.teamcode.MOEStuff.MOEBot.MOEPenCV.pipelines.MOERingPipeline
+import org.firstinspires.ftc.teamcode.autonomous.vision.MOEPipelineAssist
 import org.firstinspires.ftc.teamcode.test.rr.drive.SampleMecanumDrive
 
 @Autonomous(group = "drive")
@@ -70,7 +72,8 @@ class TestAutonomMOous : LinearOpMode() {
         arm.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
         var Config: Int = 1
 //      Temporary Config for testing
-        val ringDetectAssist = MOEPipelineAssist(x = 288, y = 6, width = 285, height = 170)
+        val pipeline = MOERingPipeline(x = 288, y = 6, width = 285, height = 170)
+        val ringDetectAssist = MOEPipelineAssist(hardwareMap, pipeline)
 
 
         val drive = SampleMecanumDrive(hardwareMap)
